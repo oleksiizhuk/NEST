052e95b6393a80c872726f89889ab4aa183b3c4f
OleksiiZhuk <oleksiizhuk.att@gmail.com>
2023-03-03 21:04:27 +0100

    fix swagger

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,24 @@ import { AppModule } from './components/app/app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import helmet from 'helmet';
 import { ValidationPipe } from '@nestjs/common';
+// import { serveStatic } from 'serve-static';
+import serveStatic = require('serve-static');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
-
+  app.use(
+    '/api',
+    serveStatic('public', {
+      index: ['index.html'],
+      setHeaders: (res, path) => {
+        if (path.endsWith('.css')) {
+          res.setHeader('Content-Type', 'text/css');
+        }
+      },
+    }),
+  );
   const options = new DocumentBuilder()
     .setTitle('Api v1')
     .setDescription('The API for vibe APP')
