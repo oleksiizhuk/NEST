4dbaf445345c0208f6be2f65335222f6ecfe1e3f
Oleksii <Oleksii.Zhuk@yum.com>
2024-04-14 08:26:14 +0200

    update

--- src/route/app/app.service.ts
+++ src/route/app/app.service.ts
@@ -5,6 +5,8 @@ import { ConfigService } from '@nestjs/config';
 export class AppService {
   constructor(private readonly configService: ConfigService) {}
   getHello(): string {
-    return `Hello World! ${this.configService.get<string>('MAIL_HOST')}`;
+    return `Hello World! ${this.configService.get<string>(
+      'MAIL_HOST',
+    )} ${this.configService.get<string>('B_API_KEY')}`;
   }
 }
